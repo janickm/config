d238ebe9a6ec122218bb0d60e85552bee48f9e87
Janick Martinez Esturo <janickm@nvidia.com>
2026-03-26 19:38:53 +0100

    feat(nvim-blink): add support for Rust fuzzy matcher

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -4,5 +4,11 @@ return {
     keymap = {
       preset = "super-tab",
     },
+    -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "prefer_rust" },
   },
 }
